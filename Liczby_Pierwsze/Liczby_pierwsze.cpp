f58fdcbe36779fbc1044f212b41330dffecfbf24
Agipcio <maku15917@gmail.com>
2022-10-09 01:01:37 +0200

    modified:   Liczby_Pierwsze/Liczby_pierwsze.cpp 	modified:   Liczby_Pierwsze/build/Debug/Liczby_pierwsze.o 	modified:   Liczby_Pierwsze/build/Debug/outDebug.exe

--- Liczby_Pierwsze/Liczby_pierwsze.cpp
+++ Liczby_Pierwsze/Liczby_pierwsze.cpp
@@ -12,5 +12,29 @@ int main()
         ciag.push_back(liczba);
         std::cout << ciag[i] << "\n";
     }
-    
+    for (int i = 0; i < 4; i++)
+    {
+        if (ciag[i] % 2 == 0 && ciag[i] != 2)
+        {
+            std::cout << "tak";
+        }
+        else
+        {
+            if (ciag[i] % 3 == 0 && ciag[i] != 3)
+            {
+                std::cout << "tak";
+            }
+            else
+            {
+                if (ciag[i] % 5 == 0 && ciag[i] != 5)
+                {
+                    std::cout << "tak";
+                }
+                else
+                {
+                    std::cout << "nie";
+                }
+            }
+        }
+    }
 }
