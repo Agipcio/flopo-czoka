5026261a277a537444ab1c0323d4e96119fc2d5a
Agipcio <maku15917@gmail.com>
2022-10-09 00:51:18 +0200

    modified:   Liczby_Pierwsze/Liczby_pierwsze.cpp	new file:   Liczby_Pierwsze/build/Debug/Liczby_pierwsze.o 	new file:   Liczby_Pierwsze/build/Debug/outDebug.exe

--- Liczby_Pierwsze/Liczby_pierwsze.cpp
+++ Liczby_Pierwsze/Liczby_pierwsze.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
-int main(){
-    moejASDFLJKHASDFHJGKLDASFLJKGHDFAGLKHJ
+#include <vector>
+int main()
+{
+    int liczba;
+    std::vector<int> ciag;
+    std::cout << "podaj liczbe: ";
+    for (int i = 0; i < 4; i++)
+    {
+        std::cin >> liczba;
+        std::cout << "\n";
+        ciag.push_back(liczba);
+        std::cout << ciag[i] << "\n";
+    }
+    
 }
