1905d1fc340e6b242b742f128ff2bc725491414e
KayKay <lotheruyt@gmail.com>
2022-10-09 01:29:28 +0200

    modified:   Liczby Pierwsze/main.cpp 	modified:   Liczby Pierwsze/main.exe

--- Liczby Pierwsze/main.cpp
+++ Liczby Pierwsze/main.cpp
@@ -3,23 +3,33 @@
 #include <cmath>
 
 int main()
-{   
-    int liczba1, liczba2, liczba3, liczba4;
-    std::cout<< "podaj 4 liczby \n";
-    std::cin>>liczba1;
-    // std::cin>>liczba2;
-    // std::cin>>liczba3;
-    // std::cin>>liczba4;
-
-    for(int i=0; i<=liczba1; i++)
+{      
+    std::vector<int> liczby;
+    bool pierwsza = true;
+    int x, y;
+    std::cout<< "podaj 4 liczby:";
+    for(int i=0; i<=3; i++)
+        {   
+            std::cin>>y;
+            // std::cout<<std::endl;
+            liczby.push_back(y);
+        }
+    for(int j=0; j==3; j++)
     {
-        if(liczba1 % i == 0)
-            std::cout<<"nie";
+        x=liczby[j];
 
-        else
-            std::cout<<"tak";
+        for(int i=2; i==x; i++)
+        {
+          if(x % i == 0)
+          pierwsza = false;
+            break;
+         }
+         if (pierwsza)
+            std::cout<<"tak \n";
+         else
+            std::cout<<"nie \n";
     }
-
+    
 
     return 0;
 }
